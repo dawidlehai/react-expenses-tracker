b0de92c642e777ae37c25cbe6f6650b078b2b821
dawidlehai <haidawidle@gmail.com>
2022-12-06 13:34:50 +0100

    Refactored expenses list into separate component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Card from "../UI/Card";
-import ExpenseItem from "./ExpenseItem";
+import ExpensesList from "./ExpensesList";
 import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
 
@@ -15,25 +15,13 @@ const Expenses = (props) => {
     (expense) => expense.date.getFullYear() === filteredYear * 1
   );
 
-  let expensesContent = <p>No expenses found.</p>;
-
-  if (filteredExpenses.length)
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
-
   return (
     <Card className="expenses">
       <ExpensesFilter
         selectedYear={filteredYear}
         onYearChange={yearChangeHandler}
       />
-      {expensesContent}
+      <ExpensesList expenses={filteredExpenses} />
     </Card>
   );
 };
